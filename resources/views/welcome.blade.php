42d87f6620a49982821ecdb06132e0554a6475bc
knone1 <knone.null@gmail.com>
2016-07-11 12:00:57 +0800

    new make:auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="padding-top: 10px;">
-  <div class="row">
-    <div class="col-sm-4">
-      <div class="card">
-  <div class="card-block">
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    Your Application's Landing Page.
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-8">
-      One of three columns
-    </div>
-  </div>
 </div>
 @endsection
