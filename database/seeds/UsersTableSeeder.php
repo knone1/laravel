353dad11c8a939d20d38ec0c0628c0cde078478a
knone1 <knone.null@gmail.com>
2016-07-16 12:33:37 +0800

    change to bcrypt

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'username' => 'admin',
-            'password' => '$2y$10$aSjo8CaykQTF4c.S0RemUuhBhTpq5.Jb8Mg.YMLGFk8wPBNDtsGW2', //pasword => 123456
+            'password' => bcrypt('123456'), //pasword => 123456
             'email' => 'admin@localhost.dev',
             'facebook_user_id' => '0',
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
