f6e29f9934e8d4b21646ea6ad54ba55597726a60
knone1 <knone.null@gmail.com>
2016-07-16 12:42:59 +0800

    add roles table seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('settings')->insert([
+            'role_user_id' => '1', //set to user faker admin
+            'role_title' => 'Administrator',
+            'role_level' => '5' //5 admin 4 moderator 3 what you want to 2 and 1, 0 = user
+        ]);
+    }
+}
